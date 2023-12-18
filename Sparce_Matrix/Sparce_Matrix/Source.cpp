e49c0ba48f4ccfac77d1044af504255d7f3e4bd0
mahmoud hatem <113213094+mahmoudhatems@users.noreply.github.com>
2023-12-18 13:12:43 +0200

    Add files via upload

---
+++ Sparce_Matrix/Sparce_Matrix/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int size = 0;
+	int sparseMatrix[4][5] =
+	{ { 0, 0, 3, 0, 4 },
+	{ 0, 0, 5, 7, 0 },
+	{ 0, 0, 0, 0, 0 },
+	{ 0, 2, 6, 0, 0 }
+	};
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 5; j++)
+			if (sparseMatrix[i][j] != 0)
+				size++;
+	int compactMatrix[3][size];
+	int k = 0;
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 5; j++)
+			if (sparseMatrix[i][j] != 0)
+			{
+				compactMatrix[0][k] = i;
+				compactMatrix[1][k] = j;
+				compactMatrix[2][k] = sparseMatrix[i][j];
+				k++;
+			}
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < size; j++)
+			cout << compactMatrix[i][j];
+		cout << "\n";
+	}
+}
